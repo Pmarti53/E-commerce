3478b173c2c5ba0a714a0bfa7270bf0785199da9
peter <ptmartinezsound@gmail.com>
2021-05-15 10:18:31 -0500

    set database models

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,8 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Comment.findAll()
+  
 });
 
 router.get('/:id', (req, res) => {
